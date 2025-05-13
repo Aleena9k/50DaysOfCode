6ac0b97abc12b7ed1407544bc1b459ac90c9227b
Aleena Khan <134202424+Aleena9k@users.noreply.github.com>
2025-05-13 21:38:13 +0530

    Create Math_1.cpp

---
+++ Day1/Math_1.cpp
@@ -0,0 +1,14 @@
+// 1822. Sign of the Product of an Array
+
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+       int sign = 1;
+       for(int num: nums){
+        if(num == 0) return 0;
+        if(num<0) sign*= -1;
+       }
+       return sign;
+    }
+};
+
